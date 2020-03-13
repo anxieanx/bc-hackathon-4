52d8be870ad6f12ca934b52bcd3efb3bfeb98c34
anxieanx <anxie@windowslive.com>
2020-03-13 10:22:35 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Display from './Display.jsx';
-import CounterButton from './CounterButton.jsx';
 
 export default class App extends Component {
 
